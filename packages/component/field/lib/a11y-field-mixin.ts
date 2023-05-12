d001029764174566ee631fa43507b3d6d1ebf4bb
christophe-g <christophe.geiser@gmail.com>
2023-05-12 08:11:37 +0200

    Internal(component): remove -a11y components We  use a variant property instead

--- packages/component/field/lib/a11y-field-mixin.ts
+++ packages/component/field/lib/a11y-field-mixin.ts
@@ -1,17 +1,19 @@
 import { LitElement, PropertyValues } from 'lit';
 import { property } from 'lit/decorators.js'
-import {Field} from './field';
+import { Field } from './field';
 
 declare global {
 	interface HTMLElementEventMap {
-		
+
 	}
 }
+
+export type Variant = 'a11y'
 
 type Constructor<T = {}> = new (...args: any[]) => T;
 export declare class A11yFieldMixinInterface {
 	// Define the interface for the mixin
-	prop: string
+	variant: Variant | undefined;
 }
 /**
  * A11yFieldMixin Mixin
@@ -22,11 +24,13 @@ export declare class A11yFieldMixinInterface {
  */
 export const A11yFieldMixin = <T extends Constructor<Field>>(superClass: T) => {
 
- 
-	class A11yFieldMixinClass extends superClass  {
-	
-	 
-		@property() prop!: String;
+
+	class A11yFieldMixinClass extends superClass {
+
+		/**
+		 * a variant of the field. Variants are use to render field differently or augment them.
+		 */
+		@property({ reflect: true }) variant!: Variant;
 
 	};
 	// Cast return type to your mixin's interface intersected with the superClass type
