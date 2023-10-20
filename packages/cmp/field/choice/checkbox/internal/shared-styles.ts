00c5a207fa5427f2ea6d722574afa8fd76fdf27b
christophe-g <christophe.geiser@gmail.com>
2023-10-20 10:13:06 +0200

    chore(cmp): fix layour space for checkbox

--- packages/cmp/field/choice/checkbox/internal/shared-styles.ts
+++ packages/cmp/field/choice/checkbox/internal/shared-styles.ts
@@ -1,7 +1,7 @@
 import { css } from 'lit';
 const styles = css`
 	*[data-role="checkbox"] {
-		margin-inline-start: 16px;
+		margin-inline-start: 4px;
 	}
 `;
 export default styles;	
--- packages/cmp/field/choice/radio/internal/shared-styles.ts
+++ packages/cmp/field/choice/radio/internal/shared-styles.ts
@@ -2,7 +2,7 @@ import { css } from 'lit';
 const styles = css`
 	*[data-role="radio"] {
 		margin-inline-end: 16px;
-		margin-inline-start: 2px;
+		margin-inline-start: 4px;
 	}
 `;
 export default styles;	
