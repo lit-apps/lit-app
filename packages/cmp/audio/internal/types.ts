e93ca6f72310c1d95f3a06dc8f4aece16d1ad750
christophe-g <christophe.geiser@gmail.com>
2024-03-20 14:32:02 +0100

    refactor(cmp): Remove unused MachineParams type

--- packages/cmp/audio/internal/types.ts
+++ packages/cmp/audio/internal/types.ts
@@ -1,13 +1,3 @@
-export type MachineParams<A extends Record<string, Record<string, any>>> =
-  keyof A extends infer Type
-  ? Type extends keyof A
-  ? keyof A[Type] extends ""
-  ? { readonly type: Type }
-  : { readonly type: Type; readonly params: A[Type] }
-  : never
-  : never;
-
-
 export interface PlayerI {
   get paused(): boolean;
   get speaking(): boolean;
