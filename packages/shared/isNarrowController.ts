1f1b4de3cd6429ac43fd10aa8674ae6275bb67d9
christophe-g <christophe.geiser@gmail.com>
2024-06-11 16:11:34 +0200

    refactor(shared): avoid multiple resize listener for isNarrowController

--- packages/shared/isNarrowController.ts
+++ packages/shared/isNarrowController.ts
@@ -1,32 +1,48 @@
 import { ReactiveController, ReactiveControllerHost } from 'lit';
+type listenerT = () => void;
+const listeners: listenerT[] = [];
+window.addEventListener('resize', () => {
+  listeners.forEach(listener => listener())
+});
+
 
 /**
- * A controller that checks if the user has scrolled to the end of the page.
+ * A controller that checks whether the screen is narrow. 
+ * 
+ * The default width is 800px.
  */
-export default class EndPageController implements ReactiveController {
-  unsubscribe!: () => void;
-  host: ReactiveControllerHost;
-  /**
-   * Whether the user has scrolled to the end of the page.
-   */
-  value!: boolean;
+export default class IsNarrowController implements ReactiveController {
+  private unsubscribe?: () => void;
   
-  constructor(host: ReactiveControllerHost, private width = 800) {
-    (this.host = host).addController(this);
+  /**
+   * Whether the width of the viewport is less than the specified width.
+   */
+  value: boolean;
+
+  /**
+   * Creates a new instance of the `IsNarrowController` class.
+   * @param host The ReactiveControllerHost instance.
+   * @param width The width threshold for determining if the viewport is narrow. Default is 800.
+   */
+  constructor(private host: ReactiveControllerHost, private width = 800) {
+    this.value = false;
+    this.host.addController(this);
+    this.check();
+  }
+
+  private check() {
+    const oldValue = this.value;
+    this.value = document.documentElement.offsetWidth < this.width;
+    if (oldValue !== this.value) {
+      this.host.requestUpdate();
+    }
   }
 
   private subscribe() {
-    const check = () => {
-      const oldValue = this.value;
-      this.value =  document.documentElement.offsetWidth < this.width;
-      if (oldValue !== this.value) {
-        this.host.requestUpdate();
-      }
-    }
-    window.addEventListener('resize', check);
-    setTimeout(check, 1000);
+    const check = () => this.check()
+    listeners.push(check);
     return () => {
-      window.removeEventListener('resize', check)
+      listeners.splice(listeners.indexOf(check), 1);
     };
   }
 
