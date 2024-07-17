d2bb9274e2fd6309a246ef880505f8d22f4eb8b7
christophe-g <christophe.geiser@gmail.com>
2024-07-17 16:15:51 +0200

    refactor(cmp): update list-item imports and renderListItem method to be able to set role to list-item

--- packages/cmp/list/list-item.ts
+++ packages/cmp/list/list-item.ts
@@ -4,11 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {customElement} from 'lit/decorators.js';
-import { css } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { css, nothing } from 'lit';
 
-import {MdListItem} from '@material/web/list/list-item';
+import { MdListItem } from '@material/web/list/list-item';
 import illustrationVariant from './internal/illustrationVariantStyles';
+import { literal, html as staticHtml, StaticValue } from 'lit/static-html.js';
+import { ARIAMixinStrict } from '@material/web/internal/aria/aria';
+import { classMap } from 'lit/directives/class-map.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -17,60 +20,67 @@ declare global {
 }
 
 /**
- * @summary
- * Lists are continuous, vertical indexes of text or images. Items are placed
- * inside the list.
- *
- * @description
- * Lists consist of one or more list items, and can contain actions represented
- * by icons and text. List items come in three sizes: one-line, two-line, and
- * three-line.
- *
- * __Takeaways:__
- *
- * - Lists should be sorted in logical ways that make content easy to scan, such
- *   as alphabetical, numerical, chronological, or by user preference.
- * - Lists present content in a way that makes it easy to identify a specific
- *   item in a collection and act on it.
- * - Lists should present icons, text, and actions in a consistent format.
- *
- * Acceptable slot child variants are:
- *
- * - `video[slot=start-video]`
- * - `video[slot=start-video-large]`
- * - `img,span[slot=start-avatar]`
- * - `img[slot=start-image]`
- * - `md-icon[slot=start-icon]`
- * - `svg[slot=start-icon]`
- * - `img[slot=start-icon]`
- * - `md-icon[slot=end-icon]`
- * - `svg[slot=end-icon]`
- * - `img[slot=end-icon]`
- *
- *  @example
- * ```html
- * <md-list-item
- *     headline="User Name"
- *     supportingText="user@name.com">
- *   <md-icon slot="start-icon">account_circle</md-icon>
- *   <md-icon slot="end-icon">check</md-icon>
- * </md-list-item>
- * ```
- *
- * @example
- *
- * @final
- * @suppress {visibility}
+ * Override of @material/web.md-list-item to add custom styles and allow to set role 
+ * attribute to listitem.
+ * 
+ * role is set via dataset.role attribute.
  */
 @customElement('lapp-list-item')
 export class LappListItem extends MdListItem {
   static override styles = [
-    ...MdListItem.styles, 
-    illustrationVariant, 
+    ...MdListItem.styles,
+    illustrationVariant,
     css`
       :host(:not([disabled])) {
         cursor: pointer;
       }`
 
   ];
+
+  /**
+ * Renders the root list item.
+ *
+ * @param content the child content of the list item.
+ */
+  protected override renderListItem(content: unknown) {
+    const isAnchor = this.type === 'link';
+    let tag: StaticValue;
+    switch (this.type) {
+      case 'link':
+        tag = literal`a`;
+        break;
+      case 'button':
+        tag = literal`button`;
+        break;
+      default:
+      case 'text':
+        tag = literal`li`;
+        break;
+    }
+
+    const isInteractive = this.type !== 'text';
+    // TODO(b/265339866): announce "button"/"link" inside of a list item. Until
+    // then all are "listitem" roles for correct announcement.
+    const target = isAnchor && !!this.target ? this.target : nothing;
+
+    // @ts-expect-error - isDisabled is private
+    const isDisabled = this.isDisabled 
+    const role = this.dataset.role || 'listitem';
+    return staticHtml`
+        <${tag}
+          id="item"
+          tabindex="${isDisabled || !isInteractive ? -1 : 0}"
+          ?disabled=${isDisabled}
+          role=${role}
+          aria-selected=${(this as ARIAMixinStrict).ariaSelected || nothing}
+          aria-checked=${(this as ARIAMixinStrict).ariaChecked || nothing}
+          aria-expanded=${(this as ARIAMixinStrict).ariaExpanded || nothing}
+          aria-haspopup=${(this as ARIAMixinStrict).ariaHasPopup || nothing}
+          class="list-item ${classMap(this.getRenderClasses())}"
+          href=${this.href || nothing}
+          target=${target}
+          @focus=${this.onFocus}
+        >${content}</${tag}>
+      `;
+  }
 }
