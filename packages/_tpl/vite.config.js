c043a7622997432dd84f448831f5662ee580b54a
christophe-g <christophe.geiser@gmail.com>
2024-08-25 13:11:10 +0200

    chore(tpl): add a template repo to copy

---
+++ packages/_tpl/vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import { litHMRPlugin } from './vite-plugin-lit-hmr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+
+  test: {
+    onConsoleLog(log) {
+      if (log.includes('Download the Vue Devtools extension')) return false
+      if (log.includes('Lit is in dev mode.')) return false
+    },
+    // include: ['./src/**/*.test.ts'],
+    browser: {
+      enabled: true,
+      name: 'chrome',
+    },
+  },
+  root: './demo',
+  plugins: [litHMRPlugin()],
+  resolve: {
+    dedupe: [
+      "router-slot",
+      "lit",
+      "api-viewer-element",
+      "loupe",  
+      "@material/web",
+      "@material/mwc-base",
+      "@material/mwc-base/form-element.js",
+      "@material/mwc-notched-outline",
+      "@material/mwc-ripple",
+      "@material/mwc-ripple/ripple-directive.js",
+      "@material/mwc-snackbar",
+      
+
+      "@material/mwc-top-app-bar",
+      "@material/mwc-top-app-bar-fixed",
+      "@preignition/preignition-util",
+      "@preignition/preignition-demo",
+      "@preignition/preignition-mixin",
+      "@preignition/preignition-config",
+      "@preignition/preignition-state",
+    ],
+  },
+});
