b031cbb34048bd2307d85c79756d92692d0397a4
christophe-g <christophe.geiser@gmail.com>
2024-10-17 14:05:19 +0200

    feat(shared): add camelToDash and dashToCamel utility functions
    Add two new utility functions, camelToDash and dashToCamel, to the shared package.

---
+++ packages/shared/camelToDash.ts
@@ -0,0 +1,17 @@
+const map:{[key:string]:string} = {};
+const CAMEL_TO_DASH = /([A-Z])/g;
+
+/**
+ * Converts a camelCase string to a dash-case string.
+ *
+ * @param camel - The camelCase string to be converted.
+ * @returns The converted dash-case string. If the input is an empty string, returns an empty string.
+ */
+export default (camel: string): string => {
+  if (!camel) {
+    return '';
+  }
+  return map[camel] || (
+    map[camel] = camel.replace(CAMEL_TO_DASH, '-$1').toLowerCase()
+  );
+};
---
+++ packages/shared/dashToCamel.ts
@@ -0,0 +1,22 @@
+const map: { [key: string]: string } = {};
+const DASH_TO_CAMEL = /-[a-z]/g;
+
+/**
+ * Converts a dash-separated string to camelCase.
+ * 
+ * This function takes a string with dash-separated words and converts it to camelCase.
+ * If the input string is empty, it returns an empty string.
+ * 
+ * @param dash - The dash-separated string to convert.
+ * @returns The camelCase version of the input string.
+ */
+export default (dash: string): string => {
+  if (!dash) {
+    return '';
+  }
+  return map[dash] || (
+    map[dash] = dash.indexOf('-') < 0 ? dash : dash.replace(DASH_TO_CAMEL,
+      (m) => m[1].toUpperCase()
+    )
+  );
+};
