0629507d2be331cf5cccd6745f8e8c61ceda4990
christophe-g <christophe.geiser@gmail.com>
2024-11-29 17:12:15 +0100

    feat(model): refactor ConsumeDataMixin =, small cleanup

--- packages/model/src/mixin/context-data-mixin.ts
+++ packages/model/src/mixin/context-data-mixin.ts
@@ -43,51 +43,51 @@ export declare class DataMixinConsumeInterface<D = any> extends DataMixinInterfa
  * }
  * ```
  */
-export const ConsumeDataMixin = <D = any>() => 
-  <T extends Constructor<ReactiveElement>>(superClass: T) => {
+export const ConsumeDataMixin = <D = any>() =>
+	<T extends Constructor<ReactiveElement>>(superClass: T) => {
 
-	abstract class ContextConsumeDataMixinClass extends superClass {
+		abstract class ContextConsumeDataMixinClass extends superClass {
 
-		@state() data!: D;
-		@property({ type: Boolean, attribute: 'prevent-consume' }) preventConsume = false;
+			@state() data!: D;
+			@property({ type: Boolean, attribute: 'prevent-consume' }) preventConsume = false;
 
-		/**
-		 * whether the data has changed for a given entity and path
-		 */
-		hasChanged(path: string, entityName: string): boolean {
-			const hasChangedEvent = new DataHasChanged(path, entityName);
-			this.dispatchEvent(hasChangedEvent);
-			return !!hasChangedEvent.detail.hasChanged;
-		}
-
-		private cachedData!: any;
-		consumer = new ContextConsumer(this, {
-			context: dataContext,
-			subscribe: true,
-			callback: (value: any) => {
-				if (this.preventConsume) {
-					this.cachedData = value;
-					// this.requestUpdate();
-				} else {
-					this.data = value;
-				}
+			/**
+			 * whether the data has changed for a given entity and path
+			 */
+			hasChanged(path: string, entityName: string): boolean {
+				const hasChangedEvent = new DataHasChanged(path, entityName);
+				this.dispatchEvent(hasChangedEvent);
+				return !!hasChangedEvent.detail.hasChanged;
 			}
-		});
 
-		override willUpdate(prop: PropertyValues) {
-			if (prop.has('preventConsume')) {
-				const old = prop.get('preventConsume');
-				if (old === false && this.preventConsume === true && this.cachedData) {
-					this.data = this.cachedData;
+			private cachedData!: any;
+			consumer = new ContextConsumer(this, {
+				context: dataContext,
+				subscribe: true,
+				callback: (value: any) => {
+					if (this.preventConsume) {
+						this.cachedData = value;
+						// this.requestUpdate();
+					} else {
+						this.data = value;
+					}
 				}
+			});
+
+			override willUpdate(prop: PropertyValues) {
+				if (prop.has('preventConsume')) {
+					const old = prop.get('preventConsume');
+					if (old === false && this.preventConsume === true && this.cachedData) {
+						this.data = this.cachedData;
+					}
+				}
+				super.willUpdate(prop);
 			}
-			super.willUpdate(prop);
-		}
 
 
-	};
-	return ContextConsumeDataMixinClass as unknown as Constructor<DataMixinConsumeInterface<D>> & T;
-}
+		};
+		return ContextConsumeDataMixinClass as unknown as Constructor<DataMixinConsumeInterface<D>> & T;
+	}
 
 /**
  * ProvideDataMixin a mixin that provides data context
@@ -112,39 +112,41 @@ export const ConsumeDataMixin = <D = any>() =>
  * }
  * ```
  */
-export const ProvideDataMixin = <D = any>() => 
-  <T extends Constructor<ReactiveElement>>(superClass: T) => {
+export const ProvideDataMixin = <D = any>() =>
+	<T extends Constructor<ReactiveElement>>(superClass: T) => {
 
-	abstract class ContextProvideDataMixinClass extends superClass {
+		abstract class ContextProvideDataMixinClass extends superClass {
 
-		@consume({ context: dataContext, subscribe: true })
-		@state() parentData!: any;
+			@consume({ context: dataContext, subscribe: true })
+			@state() parentData!: any;
 
-		@state() data!: D;
+			@state() data!: D;
 
-		provider = new ContextProvider(this, { context: dataContext, initialValue: this.data });
+			provider = new ContextProvider(this, { context: dataContext, initialValue: this.data });
 
-		override willUpdate(prop: PropertyValues) {
+			override willUpdate(prop: PropertyValues) {
 
-			// we set parentData as prototype of data if data and parentData are set
-			if (prop.has('parentData') || prop.has('data')) {
-				if (this.data === null && this.parentData) {
-					this.data = {} as D
-				}
-				if (this.data) {
-					if (Object.getPrototypeOf(this.data) === Object.prototype && this.parentData) {
-						Object.setPrototypeOf(this.data, this.parentData);
+				// we set parentData as prototype of data if data and parentData are set
+				if (prop.has('parentData') || prop.has('data')) {
+					if (this.data === null && this.parentData) {
+						this.data = {} as D
 					}
+					if (this.data && this.parentData) {
+						if (Object.getPrototypeOf(this.data) === Object.prototype) {
+							Object.setPrototypeOf(
+								this.data,
+								this.parentData)
+						} 
+					}
+					const force = Array.isArray(this.data) ? true : false;
+					this.provider.setValue(this.data, force);
+
 				}
-				const force = Array.isArray(this.data) ? true : false;
-				this.provider.setValue(this.data, force);
+				super.willUpdate(prop);
 
 			}
-			super.willUpdate(prop);
 
-		}
+		};
 
-	};
-
-	return ContextProvideDataMixinClass as unknown as Constructor<DataMixinInterface<D>> & T;
-}
+		return ContextProvideDataMixinClass as unknown as Constructor<DataMixinInterface<D>> & T;
+	}
