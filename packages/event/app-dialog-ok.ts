66e7e8acf28ed6bb6462379b1cb2e2495171e4f8
christophe-g <christophe.geiser@gmail.com>
2024-12-03 09:48:33 +0100

    feat(event): add AppDialogOkEvent class for dialog closure event

---
+++ packages/event/app-dialog-ok.ts
@@ -0,0 +1,30 @@
+// export interface appDialogOkDetail {
+// data: any;
+// }
+
+/**
+ * This event is fired when a dialog is closed with an `ok` action
+ */
+export class AppDialogOkEvent<D = any> extends CustomEvent<D> {
+  static readonly eventName = 'app-dialog-ok`';
+  constructor(detail: D) {
+    super(AppDialogOkEvent.eventName, {
+      bubbles: true,
+      composed: true,
+      detail: detail
+    });
+  }
+}
+
+export function AppDialogOkEventFactory<T>() {
+  return class TypedAppDialogOkEvent extends AppDialogOkEvent<T> {
+  };
+}
+
+declare global {
+  interface HTMLElementEventMap {
+    [AppDialogOkEvent.eventName]: AppDialogOkEvent<any>,
+  }
+}
+
+export default AppDialogOkEvent
