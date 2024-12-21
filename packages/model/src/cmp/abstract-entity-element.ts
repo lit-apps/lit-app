201d4260fa624b83d3f02575ab2a03fb2ca0b846
christophe-g <christophe.geiser@gmail.com>
2024-12-21 17:37:26 +0100

    refactor(model): comment out data loading message for clarity

--- packages/model/src/cmp/abstract-entity-element.ts
+++ packages/model/src/cmp/abstract-entity-element.ts
@@ -142,9 +142,9 @@ export default abstract class AbstractEntityElement extends
 			return html`getting entity...`;
 		}
 
-		if (this.data === undefined && this.consumingMode !== 'print' && this.consumingMode !== 'offline') {
-			return html`getting data...`;
-		}
+		// if (this.data === undefined && this.consumingMode !== 'print' && this.consumingMode !== 'offline') {
+		// 	return html`getting data...`;
+		// }
 
 		return [
 			super.render(),
