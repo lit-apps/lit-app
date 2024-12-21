feddba703e995e99df4075d84e6506c282d2638f
christophe-g <christophe.geiser@gmail.com>
2024-12-21 17:38:51 +0100

    feat(cmp): replace PwiTooltip with Vaadin Tooltip and update example usage

--- packages/cmp/tooltip/tooltip.ts
+++ packages/cmp/tooltip/tooltip.ts
@@ -1,20 +1,32 @@
-import PwiTooltip from "@preignition/pwi-tooltip/src/pwi-tooltip.js";
+// import PwiTooltip from "@preignition/pwi-tooltip/src/pwi-tooltip.js";
 import { customElement } from 'lit/decorators.js';
+import '@vaadin/tooltip/theme/lumo/vaadin-lit-tooltip.js';
+import {Tooltip} from '@vaadin/tooltip/src/vaadin-lit-tooltip.js';
+
+import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+const tooltipOverlay = css`
+  [part='overlay'] {
+    background: var(--color-inverse-surface) linear-gradient(var(--lumo-contrast-5pct), var(--lumo-contrast-5pct));
+    color: var(--color-inverse-on-surface);
+  }
+`;
+
+registerStyles('vaadin-tooltip-overlay', [tooltipOverlay], { moduleId: 'lumo-tooltip-overlay' });
 
 /**
  * A tooltip component.
  * 
  * @example 
  * ```html
- * <pwi-tooltip skipFocus discrete message="tooltip" position="bottom">
- *   <md-filled-icon-button toggle aria-label="Label"
- *     <lapp-icon>format_bold</lapp-icon>
- *   </md-filled-icon-button>
- * </pwi-tooltip>
+ * <lapp-tooltip text="tooltip" for="button" ></lapp-tooltip>
+ * <md-filled-icon-button id="button" toggle aria-label="Label"
+ *   <lapp-icon>format_bold</lapp-icon>
+ * </md-filled-icon-button>
  * ``` 
  */
+// @ts-expect-error = Tooltip types not properly exported
 @customElement('lapp-tooltip')
-export default class lappTooltip extends PwiTooltip {
+export default class lappTooltip extends Tooltip {
 
 }
 
