0c0723f83a71448d428d4a3db88da4a1ab2e5f0c
christophe-g <christophe.geiser@gmail.com>
2024-12-22 11:39:48 +0100

    feat(shared): add native currency and language mappings

---
+++ packages/shared/locale/currency-native.ts
@@ -0,0 +1,11 @@
+// sourcehttps://en.wikipedia.org/wiki/List_of_language_names
+
+type Native = {[key: string]: string};
+
+export const native: Native = {
+  eur: 'Euro (€)',
+  usd: 'United States Dollar ($)',
+  gbp: 'British Pound Sterling (£)',
+};
+
+export default native;
