aeacfad3f1229669fc489c41476846e219778bfc
christophe-g <christophe.geiser@gmail.com>
2024-12-24 17:18:09 +0100

    refactor(shared)make doc appear in editor

--- packages/shared/getInnerText.ts
+++ packages/shared/getInnerText.ts
@@ -1,3 +1,5 @@
+const div = document.createElement('div');
+
 /**
  * Converts an HTML string to plain text by setting the innerHTML of a temporary div element
  * and then retrieving its innerText.
@@ -5,8 +7,6 @@
  * @param label - The HTML string to be converted to plain text. Defaults to an empty string.
  * @returns The plain text representation of the provided HTML string.
  */
-const div = document.createElement('div');
-
 export default (label: string = ''): string => {
   div.innerHTML = label;
   return div.innerText;
