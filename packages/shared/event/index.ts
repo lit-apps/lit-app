ca302e7f47b8fc04ca5078f404221e6f73c88aa8
christophe-g <christophe.geiser@gmail.com>
2025-01-08 10:12:06 +0100

    fix(event): update import path for ValueChangedEvent to reflect new file structure

--- packages/shared/event/index.ts
+++ packages/shared/event/index.ts
@@ -1,6 +1,6 @@
 import ToastEvent from './toast-event';
 import UICloseEvent from './ui-event';
-import ValueChangedEvent from './value-changed-event';
+import ValueChangedEvent from './value-changed';
 export {
 	ToastEvent,
 	ValueChangedEvent,
