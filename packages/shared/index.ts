a00da99b55fc69fc79be01d592f788c892fa7399
christophe-g <christophe.geiser@gmail.com>
2025-01-12 09:54:23 +0100

    feat(shared): export deep get/set functions for enhanced property access

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -2,3 +2,4 @@ export { wait } from './wait';
 export { debounce } from './debounce';
 export { throttle } from './throttle';
 export { interceptMethod } from './support/method-interception';
+export { get, set, deep } from './deep';
