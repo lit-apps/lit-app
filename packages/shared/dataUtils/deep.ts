79bafb84a86e8f1b9811eeebedc64fd401e6cb85
christophe-g <christophe.geiser@gmail.com>
2025-04-03 12:54:43 +0200

    feat(shared): add deepPure function for immutable deep actions on objects

--- packages/shared/dataUtils/deep.ts
+++ packages/shared/dataUtils/deep.ts
@@ -18,6 +18,40 @@ export const deep = (action: ActionT, obj: unknown, keys: string | string[], id?
   return action(obj, id as unknown as string);
 };
 
+
+/**
+ * Applies a deep action on an object based on a given path, but maintains immutability.
+ * Creates new objects along the path instead of mutating the original.
+ * 
+ * @param action - The action to be applied on the object.
+ * @param obj - The object to apply the action on.
+ * @param keys - The path to the property in the object.
+ * @returns A new object with the action applied at the specified path.
+ */
+export const deepPure = (action: ActionT, obj: unknown, keys: string | string[]): any => {
+  keys = Array.isArray(keys) ? keys : keys.split('.');
+  if (keys.length === 0) {
+    return obj;
+  }
+
+  const [currentKey, ...remainingKeys] = keys;
+
+  if (remainingKeys.length === 0) {
+    // We've reached the end of the path, apply the action
+    return action(obj, currentKey)
+    //   ...(obj as object || {}),
+    //   [currentKey]: 
+    // };
+  }
+
+  // Continue down the path with a recursive call
+  return deepPure(
+    action,
+    (obj as any)?.[currentKey] || {},
+    remainingKeys
+  )
+};
+
 const _get = (obj: any, prop: string) => obj[prop];
 const _set = (n: any) => (obj: any, prop: string) => (obj[prop] = n);
 // const _setOwnProperty = (n: any) => (obj: any, prop: string) => Object.defineProperty(obj, prop, { value: n, writable: true, enumerable: true, configurable: true });
@@ -29,7 +63,7 @@ const _set = (n: any) => (obj: any, prop: string) => (obj[prop] = n);
  * @returns the value of the property at the given path
  */
 // export const get = <D extends object, P extends NestedKeys<D>>(path: P, obj: D): NestedValue<D,P> => deep(_get, obj, path);
-export const get = <D extends object>(path: NestedKeys<D>, obj: D) => deep(_get, obj, path);
+export const get = <D extends object>(path: NestedKeys<D>, obj: D) => deepPure(_get, obj, path);
 
 /**
  * set a deep nested property
