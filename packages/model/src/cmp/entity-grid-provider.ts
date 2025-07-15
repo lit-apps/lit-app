a7f1be39b292b782b7b33926c63f34e6666ffc1d
christophe-g <christophe.geiser@gmail.com>
2025-07-15 11:45:09 +0200

    fix(shared/cmp/entity-grid-provider): correct typos in comments for clarity

--- packages/model/src/cmp/entity-grid-provider.ts
+++ packages/model/src/cmp/entity-grid-provider.ts
@@ -5,7 +5,6 @@ import { css, TemplateResult } from "lit";
 import { customElement, property, query } from 'lit/decorators.js';
 import { entityI, RenderConfig } from "../types.js";
 import AbstractEntityElement from "./abstract-entity-element.js";
-
 /**
  * an element that display an entity grid and reloads data on demand
  * 
@@ -25,12 +24,12 @@ export default class lappEntityGridProvider extends AbstractEntityElement {
       await wait(50);
     }
     if (this.grid) {
-      // @ts-expect-error - for the time being, this is only nmplemented for  Option
+      // @ts-expect-error - for the time being, this is only implemented for  Option
       if (!this.entity.getGridData) {
         console.warn('getGridData not implemented for', this.entity);
         return;
       }
-      // @ts-expect-error - for the time being, this is only nmplemented for  Option
+      // @ts-expect-error - for the time being, this is only implemented for  Option
       this.items = await (this.entity).getGridData(this.data, this.language);
       this.grid.items = this.items;
     }
