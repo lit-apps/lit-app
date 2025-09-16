2fe2811ec82385b751dccd6e11ae02e21788d756
christophe-g <christophe.geiser@gmail.com>
2025-09-16 13:43:20 +0200

    fix(cmp/dialog-download): comment out static styles override for clarity

--- packages/cmp/dialog/dialog-download.ts
+++ packages/cmp/dialog/dialog-download.ts
@@ -3,11 +3,11 @@ import { customElement } from 'lit/decorators.js';
 export { default as DownloadEvent } from './event';
 @customElement('lapp-dialog-download')
 export class LappDialogDownload extends DialogDownload {
-  static override styles = DialogDownload.styles;
+  // static override styles = DialogDownload.styles;
 }
 
 declare global {
   interface HTMLElementTagNameMap {
     'lapp-dialog-download': LappDialogDownload;
   }
-}
+} 
